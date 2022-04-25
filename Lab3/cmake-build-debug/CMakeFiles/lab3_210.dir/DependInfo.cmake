c9ee26b22e1fdc02422ecb972f2c532ac3da824a
rollcorn <lazydaimon@gmail.com>
2022-04-25 19:29:36 +0300

    Update structure

---
+++ Lab3/cmake-build-debug/CMakeFiles/lab3_210.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rollcorn/Programming/CPP/C210-CPP-OOP/Lab3/lab_vector_list_deque.cpp" "/home/rollcorn/Programming/CPP/C210-CPP-OOP/Lab3/cmake-build-debug/CMakeFiles/lab3_210.dir/lab_vector_list_deque.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab3/cmake-build-debug/CMakeFiles/lab3_210.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_210.dir/lab_vector_list_deque.cpp.o"
+  "lab3_210"
+  "lab3_210.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_210.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
